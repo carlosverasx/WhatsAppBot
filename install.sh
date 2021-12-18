4d6ffc8e3d055a074f8614cdbd44a642ea6386b9
Carlos Augusto Veras <59514129+carlosaugustoveras@users.noreply.github.com>
2021-12-17 22:15:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,13 +21,13 @@ fi
 
 if [ "$1" != "-yt" ]; then  # if you don't want to install yt-dlp, use the -yt flag
     echo "Instalando yt-dlp..."
-    curl https://gist.githubusercontent.com/kamuridesu/b56067968e154f16bfd1af6bde18e929/raw/8b32b4390983359b60941f429083daf5ee51aacf/yt-dlp.sh | bash
+    curl https://gist.githubusercontent.com/carlosaugustoveras/b56067968e154f16bfd1af6bde18e929/raw/8b32b4390983359b60941f429083daf5ee51aacf/yt-dlp.sh | bash
 fi
 
 # check if current folder is not a git repository
 if [ ! -d .git ]; then
     echo "Baixando bot..."
-    git clone https://github.com/kamuridesu/WhatsappBot.git 2>&1>/dev/null
+    git clone https://github.com/carlosaugustoveras/WhatsappBot.git 2>&1>/dev/null
     cd WhatsappBot
 fi
 
